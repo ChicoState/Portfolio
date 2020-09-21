983379c4d9c160e153bd2f7423ef7f2f01a62437
Devan Hedglin <devan@mbp.local>
2020-09-21 15:00:25 -0700

    added postgres docker container and linked it to express server Closes: #19

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: "postgres",
+    host: "db",
+    port: 5432,
+    database: "portfolio"
+});
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pool = require("./db");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/greeting', (req, res) => {
-    res.json({message: "Hello World"});
-    console.log("Hello");
+  pool.connect().then((con) => {
+    res.json({message: "Sucessfully connecetd to database!"});
+    console.log("Sucessfully connecetd to database!");
+  })
 });
 
 app.listen(3001, () =>
