6a9821cf0af4a7afad13f32712664927b9221825
lwzearfoss <lwzearfoss@gmail.com>
2020-09-30 20:11:12 -0700

    Adds basic user creation and authentication.

---
+++ server/src/models/user.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const beautifyUnique = require('mongoose-beautiful-unique-validation');
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "can't be blank"],
+    unique: [true, 'username ({VALUE}) is already taken'],
+    match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+    index: true,
+    min: 3,
+    max: 15,
+  },
+  email: {
+    type: String,
+    required: [true, "can't be blank"],
+    unique: [true, 'already taken'],
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+    index: true,
+    max: 1024,
+  },
+  password: {
+    type: String,
+    required: [true, "can't be blank"],
+    min: 8,
+    max: 1024,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    required: true,
+  },
+  first_name: {
+    type: String,
+    match: [/^[a-zA-Z]+$/, 'is invalid'],
+    index: true,
+    max: 1024,
+  },
+  middle_name: {
+    type: String,
+    match: [/^[a-zA-Z]+$/, 'is invalid'],
+    index: true,
+    max: 1024,
+  },
+  last_name: {
+    type: String,
+    match: [/^[a-zA-Z]+$/, 'is invalid'],
+    index: true,
+    max: 1024,
+  },
+});
+
+UserSchema.plugin(beautifyUnique);
+
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  bcrypt.hash(this.password, 10, (err, passwordHash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = passwordHash;
+    next();
+  });
+});
+
+UserSchema.methods.comparePassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) {
+      return cb(err);
+    }
+    if (!isMatch) {
+      return cb(null, isMatch);
+    }
+    return cb(null, this);
+  });
+};
+
+module.exports = mongoose.model('User', UserSchema);
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,89 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const cors = require('cors');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const User = require('./models/user');
 
+require('dotenv').config();
+require('./passportConfig')(passport);
 
 const app = express();
+
+mongoose.connect(
+  `${process.env.MONGO_URL}`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log('Mongoose is connected.');
+  }
+);
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'secretcode',
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
+app.use(cookieParser('secretcode'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user) => {
+    if (err) {
+      throw err;
+    }
+    if (!user) {
+      res.send('No user exists!');
+    } else {
+      req.login(user, (error) => {
+        if (error) {
+          throw error;
+        }
+        res.send('Successfully authenticated!');
+        console.log(req.user);
+      });
+    }
+  })(req, res, next);
+});
+
+app.post('/register', async (req, res) => {
+  const newUser = new User({
+    username: req.body.registerUsername,
+    password: req.body.registerPassword,
+    email: req.body.registerEmail,
+    role: 'user',
+    first_name: req.body.registerFirstName,
+    middle_name: req.body.registerMiddleName,
+    last_name: req.body.registerLastName,
+  });
+  await newUser.save((err) => {
+    if (err) {
+      res.json(err);
+    }
+    return res.send(`User ${req.body.registerUsername} successfully created.`);
+  });
+});
+
+app.get('/user', (req, res) => {
+  res.send(req.user);
+});
 
 app.get('/api/greeting', (req, res) => {
   mongoose.connect(`${process.env.MONGO_URL}`).then(() => {
