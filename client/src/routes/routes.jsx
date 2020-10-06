848373f5579a95ac4b42023dd3eb5da363acc277
Colton0012 <coltond0012@gmail.com>
2020-10-05 20:42:59 -0700

    Fixed linting issues

---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import SignUp from '../components/screens/signup';
+import Login from '../components/screens/login';
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    );
+  }
+}
