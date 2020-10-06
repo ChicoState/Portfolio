00417917c0e94c0710870b23bea1d45c3272d57f
Colton0012 <coltond0012@gmail.com>
2020-10-06 00:07:00 -0700

    added travis ci test

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = ReactDOM.render(
+    <BrowserRouter>
+      <div>
+        <App />
+      </div>
+    </BrowserRouter>,
+  );
   const linkElement = getByText(/Learn React/i);
   expect(linkElement).toBeInTheDocument();
 });
