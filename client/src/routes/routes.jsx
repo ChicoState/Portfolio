5261dac5af403c06990db1cd8abfb359fd9f8766
Colton0012 <coltond0012@gmail.com>
2020-10-06 23:20:27 -0700

    Made changes to routes file

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import { Switch, Route, Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import SignUp from '../components/screens/signup';
 import Login from '../components/screens/login';
-import history from './history'
+
 
 export default class Routes extends Component {
   render() {
     return (
-        <Router history = {history} >
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={SignUp} />
-            </Switch>
-        </Router>
+        <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+        </Switch>
     );
   }
 }
