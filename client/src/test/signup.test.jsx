17931eb63b5329db8f895f82ff32e16082b753c1
Joseph-Levin <joseph.levin99@gmail.com>
2020-10-17 00:15:54 -0700

    Hopefully fixing signup test

--- client/src/test/signup.test.jsx
+++ client/src/test/signup.test.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import SignUp from '../components/screens/signup';
 
 test('sign up test', () => {
-  const { getByText } = render(<SignUp />);
-  const linkElement = getByText(/Sign Up/i);
+  const { getAllByText } = render(<SignUp />);
+  const linkElement = getAllByText(/Sign Up/i);
   expect(linkElement).toBeInTheDocument();
 });
