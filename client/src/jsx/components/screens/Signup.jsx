248742fa8b9cac8de69ddec59135804eed7f9113
Devan Hedglin <dlhedglin@mail.csuchico.edu>
2020-12-06 17:18:14 -0800

    removed debug display

--- client/src/jsx/components/screens/Signup.jsx
+++ client/src/jsx/components/screens/Signup.jsx
@@ -142,8 +142,8 @@ class Signup extends Component {
                   </Button>
                 )}
 
-                <pre>{JSON.stringify(values, null, 2)}</pre>
-                <pre>{JSON.stringify(errors, null, 2)}</pre>
+                {/* <pre>{JSON.stringify(values, null, 2)}</pre> */}
+                {/* <pre>{JSON.stringify(errors, null, 2)}</pre> */}
               </Form>
             );
           }}
