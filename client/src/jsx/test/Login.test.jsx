8e1e57efc5375e6ad110e15160c236333c741523
Devan Hedglin <dlhedglin@mail.csuchico.edu>
2020-12-06 20:23:53 -0800

    testing for login form

--- client/src/jsx/test/Login.test.jsx
+++ client/src/jsx/test/Login.test.jsx
@@ -1,17 +1,39 @@
-/*
 import React from 'react';
-import { render } from '@testing-library/react';
-import Login from '../components/screens/Login';
+import Login from './components/Login';
+import Adapter from 'enzyme-adapter-react-16';
+import { mount, shallow, configure } from 'enzyme';
 
-test('login test', () => {
-  const { getByText } = render(<Login />);
-  const linkElement = getByText(/Login/i);
-  expect(linkElement).toBeInTheDocument();
-});
-*/
+configure({ adapter: new Adapter() });
 
-const sum = require('./sum');
+describe('Login', () => {
+  test('Render 2 input fields', () => {
+    const wrapper = mount(<Login />);
+    expect(wrapper.find('input').length).toBe(2);
+  });
+  test('init fields blank', () => {
+    const wrapper = mount(<Login />);
+    const emailInput = wrapper.find('input').first();
+    const passwordInput = wrapper.find('input').at(1);
+    expect(emailInput.props().value).toEqual('');
+    expect(passwordInput.props().value).toEqual('');
+  });
+  test('blank form submit', ()=> {
+    const wrapper = mount(<Login />);
+    const btn = wrapper.find('.btn');
+    expect(btn.length).toBe(1);
+    btn.simulate('click');
+    wrapper.update();
+    expect(wrapper.find('.invalid-feedback').first().text()).toBe('Please enter a valid email');
+  });
+  test('form submit', ()=> {
+    const wrapper = mount(<Login />);
+    const emailInput = wrapper.find('input').first();
+    const passwordInput = wrapper.find('input').at(1);
+    emailInput.simulate('change', { target: { value: 'Hello@gmail.com' } })
+    passwordInput.simulate('change', { target: { value: 'Hello' } })
+    wrapper.find('button').simulate('click');
+    wrapper.update();
+    expect(wrapper.find('.invalid-feedback').length).toBe(0);
+  });
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
 });
